2400f54e7983795492d74a39afd818941688ebf0
apurvak1309 <apurvak1309@email.com>
2023-06-10 14:32:04 +0530

    lkj

---
+++ src/component/MedicineList.js
@@ -0,0 +1,30 @@
+let medicine=[
+    {
+        "name":"Thorazine"
+    },{
+        "name":"Trilafon"
+    },
+    {
+        "name":"Stelazine"
+
+    },
+    {
+        "name":"Serentil"
+    },
+    {
+        "name":"Prolixin"
+
+    },
+    {
+        "name":"Navane"
+    },{
+        "name":"Moban"
+    },{
+        "name":"Mellaril "
+    },{
+        "name":"Haldol"
+    },
+
+
+    
+]
